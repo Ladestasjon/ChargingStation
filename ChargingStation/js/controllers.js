96039d1ce329572f3322f98325a795f44342d2b7
andersem <andersems@gmail.com>
2014-03-29 11:40:12 +0100

    Updatez

--- ChargingStation/js/controllers.js
+++ ChargingStation/js/controllers.js
@@ -1,12 +1,18 @@
 var cs = angular.module('app', []);
 
-cs.controller('csCtrl', function ($scope) {
-    $scope.phones = [
-        {'name': 'Nexus S',
-            'snippet': 'Fast just got faster with Nexus S.'},
-        {'name': 'Motorola XOOM™ with Wi-Fi',
-            'snippet': 'The Next, Next Generation tablet.'},
-        {'name': 'MOTOROLA XOOM™',
-            'snippet': 'The Next, Next Generation tablet.'}
-    ];
+cs.controller('csCtrl', function ($scope, $http, $log) {
+    $scope.ladestasjoner = $http.jsonp('http://nobil.no/api/server/search.php?callback=JSON_CALLBACK',
+        { params: {
+            apikey: '17a7a832c2e7bb2b593fbd8f0f68906b',
+            apiversion: '3',
+            action: "search",
+            type: 'rectangle',
+            northeast: '(59.943921193288915, 10.826683044433594)',
+            southwest: '(59.883683240905256, 10.650901794433594)',
+            existingids: '189,195,199,89,48'
+        }});
+
+    $scope.ladestasjoner.success(function (data) {
+            $scope.chargerstations = data.chargerstations;
+        });
 });
