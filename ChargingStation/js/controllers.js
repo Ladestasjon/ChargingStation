7c777139630aab2adfbd47bfe2cd719dcdfc4a7a
andersem <andersems@gmail.com>
2014-03-29 10:37:48 +0100

    Updatez

---
+++ ChargingStation/js/controllers.js
@@ -0,0 +1,12 @@
+var cs = angular.module('app', []);
+
+cs.controller('csCtrl', function ($scope) {
+    $scope.phones = [
+        {'name': 'Nexus S',
+            'snippet': 'Fast just got faster with Nexus S.'},
+        {'name': 'Motorola XOOM™ with Wi-Fi',
+            'snippet': 'The Next, Next Generation tablet.'},
+        {'name': 'MOTOROLA XOOM™',
+            'snippet': 'The Next, Next Generation tablet.'}
+    ];
+});
---
+++ ChargingStation/test/unit/controllersSpec.js
@@ -0,0 +1,12 @@
+describe('ChargingStation controllers', function() {
+
+    describe('csCtrl', function(){
+
+        it('should create "phones" model with 3 phones', function() {
+            var scope = {},
+                ctrl = new PhoneListCtrl(scope);
+
+            expect(scope.phones.length).toBe(3);
+        });
+    });
+});
